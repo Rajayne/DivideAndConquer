852c9d7d2f226791138925e6b1a478afb95654d2
Rajayne <ryann.j.nealey@gmail.com>
2023-05-25 10:58:49 -0700

    Create count-zeroes function and pass tests

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,14 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) count += 1;
+  }
+  return count;
 }
 
-module.exports = countZeroes
+module.exports = countZeroes;
+
+countZeroes([1, 1, 1, 1, 0, 0]); // 2
+countZeroes([1, 0, 0, 0, 0]); // 4
+countZeroes([0, 0, 0]); // 3
+countZeroes([1, 1, 1, 1]); // 0
