989e4556b4b61b600e1b8859472c23d8ad90ab2e
Rajayne <ryann.j.nealey@gmail.com>
2023-05-25 12:23:38 -0700

    Refactor countZeroes with constraint O(log n)

--- count-zeroes.test.js
+++ count-zeroes.test.js
@@ -1,10 +1,10 @@
-const countZeroes = require("./count-zeroes");
+const { countZeroes, countZeroesConstraint } = require("./count-zeroes");
 
 describe("#countZeroes", function () {
   it("counts the number of zeroes", function () {
-    expect(countZeroes([1, 1, 1, 1, 0, 0])).toBe(2);
-    expect(countZeroes([1, 0, 0, 0, 0])).toBe(4);
-    expect(countZeroes([0, 0, 0])).toBe(3);
-    expect(countZeroes([1, 1, 1, 1])).toBe(0);
+    expect(countZeroesConstraint([1, 1, 1, 1, 0, 0])).toBe(2);
+    expect(countZeroesConstraint([1, 0, 0, 0, 0])).toBe(4);
+    expect(countZeroesConstraint([0, 0, 0])).toBe(3);
+    expect(countZeroesConstraint([1, 1, 1, 1])).toBe(0);
   });
 });
