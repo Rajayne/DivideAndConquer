8096f51f7735ed92962b069a34015d46f7580021
Rajayne <ryann.j.nealey@gmail.com>
2023-05-25 11:03:05 -0700

    Create sortedFrequency function and pass tests

--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,5 +1,13 @@
-function sortedFrequency() {
-
+function sortedFrequency(arr, val) {
+  let frequency = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) frequency += 1;
+  }
+  if (frequency === 0) {
+    return -1;
+  } else {
+    return frequency;
+  }
 }
 
-module.exports = sortedFrequency
+module.exports = sortedFrequency;
