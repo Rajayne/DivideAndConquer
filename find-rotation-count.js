400113d02c5b48fa85be3bf1d5b8709c748b9205
Rajayne <ryann.j.nealey@gmail.com>
2023-05-25 11:38:09 -0700

    Create findRotationCount function and pass tests

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,3 +1,13 @@
-function findRotationCount() {}
+function findRotationCount(arr) {
+  let leftIdx = 0;
+  let rightIdx = arr.length - 1;
+  while (leftIdx <= rightIdx) {
+    if (arr[leftIdx] < arr[rightIdx] || arr[leftIdx] === arr[rightIdx]) {
+      return leftIdx;
+    } else {
+      leftIdx += 1;
+    }
+  }
+}
 
 module.exports = findRotationCount;
