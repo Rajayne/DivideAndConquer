c7186ae73cb07d79ac0e91f35db4f9a055b53888
Rajayne <ryann.j.nealey@gmail.com>
2023-05-25 11:53:48 -0700

    Refactor findFloor with constraint O(log n)

--- find-floor.js
+++ find-floor.js
@@ -10,7 +10,24 @@ function findFloor(arr, val) {
   return -1;
 }
 
-module.exports = findFloor;
+function findFloorConstraint(arr, val, low = 0, high = arr.length - 1) {
+  if (low > high) return 0;
+  if (val >= arr[high]) return arr[high];
+
+  let mid = Math.floor((low + high) / 2);
+  if (arr[mid] === val) return arr[mid];
+
+  if (mid > 0 && arr[mid - 1] <= val && val < arr[mid]) {
+    return arr[mid - 1];
+  }
+
+  if (val < arr[mid]) {
+    return findFloor(arr, val, mid - 1, high);
+  }
+  return findFloor(arr, val, mid + 1, high);
+}
+
+module.exports = { findFloor, findFloorConstraint };
 
 // findFloor([1, 2, 8, 10, 10, 12, 19], 9); // 8
 // findFloor([1, 2, 8, 10, 10, 12, 19], 20); // 19
