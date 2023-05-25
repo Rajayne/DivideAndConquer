9a95763c795acc9de196fe2a9714bf73e135a0bd
Rajayne <ryann.j.nealey@gmail.com>
2023-05-25 12:26:04 -0700

    Refactor sortedFrequency with constraint O(log n)

--- sorted-frequency.test.js
+++ sorted-frequency.test.js
@@ -1,10 +1,13 @@
-const sortedFrequency = require("./sorted-frequency")
+const {
+  sortedFrequency,
+  sortedFrequencyConstraint,
+} = require("./sorted-frequency");
 
-describe("#sortedFrequency", function(){
-  it("returns the frequency", function(){
-    expect(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 2)).toBe(4)
-    expect(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 3)).toBe(1)
-    expect(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 1)).toBe(2)
-    expect(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 4)).toBe(-1)
-  })
-})
+describe("#sortedFrequency", function () {
+  it("returns the frequency", function () {
+    expect(sortedFrequencyConstraint([1, 1, 2, 2, 2, 2, 3], 2)).toBe(4);
+    expect(sortedFrequencyConstraint([1, 1, 2, 2, 2, 2, 3], 3)).toBe(1);
+    expect(sortedFrequencyConstraint([1, 1, 2, 2, 2, 2, 3], 1)).toBe(2);
+    expect(sortedFrequencyConstraint([1, 1, 2, 2, 2, 2, 3], 4)).toBe(-1);
+  });
+});
