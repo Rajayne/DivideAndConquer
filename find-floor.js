4b6d4dc78b3f1b9f2f0cea9fe1facdc1b803dd82
Rajayne <ryann.j.nealey@gmail.com>
2023-05-25 11:44:45 -0700

    Create findFloor function and pass tests

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,17 @@
-function findFloor() {
-  
+function findFloor(arr, val) {
+  let rightIdx = arr.length - 1;
+  while (rightIdx >= 0) {
+    if (arr[rightIdx] < val) {
+      return arr[rightIdx];
+    } else {
+      rightIdx -= 1;
+    }
+  }
+  return -1;
 }
 
-module.exports = findFloor
+module.exports = findFloor;
+
+// findFloor([1, 2, 8, 10, 10, 12, 19], 9); // 8
+// findFloor([1, 2, 8, 10, 10, 12, 19], 20); // 19
+// findFloor([1, 2, 8, 10, 10, 12, 19], 0); // -1
